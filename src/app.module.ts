a96e14faf04c62e6f2162d61fe9076b28722dcd2
Marcos Flavio <mfghost69@gmail.com>
2025-02-05 00:16:48 -0300

    feat: add configModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { UsersModule } from './modules/users/users.module'
 
+import { isProduction } from '@/helper/environment'
+
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      ignoreEnvFile: isProduction
+    })
+  ],
   controllers: [AppController],
   providers: [AppService]
 })
